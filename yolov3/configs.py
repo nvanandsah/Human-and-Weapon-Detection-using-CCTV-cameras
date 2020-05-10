f7e51cf329f8b9e0ca9ab5891a3d4df9fd2bb446
nvanandsah <navneet16170@iiitd.ac.in>
2020-05-10 14:42:11 +0530

    Initial Commit

---
+++ yolov3/configs.py
@@ -0,0 +1,43 @@
+#================================================================
+#
+#   File name   : configs.py
+#   Author      : PyLessons
+#   Created date: 2020-04-20
+#   Website     : https://pylessons.com/
+#   GitHub      : https://github.com/pythonlessons/TensorFlow-2.x-YOLOv3
+#   Description : yolov3 configuration file
+#
+#================================================================
+
+# YOLO options
+YOLO_DARKNET_WEIGHTS        = "./model_data/yolov3.weights"
+YOLO_COCO_CLASSES           = "./model_data/coco.names"
+YOLO_STRIDES                = [8, 16, 32]
+YOLO_IOU_LOSS_THRESH        = 0.5
+YOLO_ANCHOR_PER_SCALE       = 3
+YOLO_MAX_BBOX_PER_SCALE     = 100
+YOLO_INPUT_SIZE             = 416
+YOLO_ANCHORS                = [[[10,  13], [16,   30], [33,   23]],
+                               [[30,  61], [62,   45], [59,  119]],
+                               [[116, 90], [156, 198], [373, 326]]]
+# Train options
+TRAIN_CLASSES               = "./mnist/mnist.names"
+TRAIN_ANNOT_PATH            = "./mnist/mnist_train.txt"
+TRAIN_LOGDIR                = "./log"
+TRAIN_BATCH_SIZE            = 4
+TRAIN_INPUT_SIZE            = 416
+TRAIN_DATA_AUG              = True
+TRAIN_TRANSFER              = False
+TRAIN_LR_INIT               = 1e-4
+TRAIN_LR_END                = 1e-6
+TRAIN_WARMUP_EPOCHS         = 2
+TRAIN_EPOCHS                = 30
+
+# TEST options
+TEST_ANNOT_PATH             = "./mnist/mnist_test.txt"
+TEST_BATCH_SIZE             = 4
+TEST_INPUT_SIZE             = 416
+TEST_DATA_AUG               = False
+TEST_DECTECTED_IMAGE_PATH   = "./data/detection/"
+TEST_SCORE_THRESHOLD        = 0.3
+TEST_IOU_THRESHOLD          = 0.45
